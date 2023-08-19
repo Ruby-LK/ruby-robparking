10c9443afc0da90b46f49eb29bb29c964645fe5e
Scar <52679146+scarfoxed@users.noreply.github.com>
2023-08-19 12:03:07 -0400

    Update config.lua
    ox support/ox item

--- config.lua
+++ config.lua
@@ -1,10 +1,12 @@
 Config = {}
 
-Config.MoneyType = 'cash' -- 'cash' = cash reward / 'markedmoney' = for marked-bills
-Config.MinReward = 100 -- Minimum reward amount
-Config.MaxReward = 400 -- Maximum reward amount
+Config.Target = 'ox' -- ox or qb
+Config.Inventory = 'ox' -- ox or qb
+Config.MoneyType = 'markedbills' -- 'cash' = cash reward / 'markedmoney' = for qb marked-bills / 'markedbills' for ox
+Config.MinReward = 5 -- Minimum reward amount
+Config.MaxReward = 20 -- Maximum reward amount
 Config.Cooldown = 5000 -- Cooldown time in milliseconds
 
-Config.dispatch = true -- true = Enables the dispatch alerts / false = Disables the alerts.
+Config.dispatch = false -- true = Enables the dispatch alerts / false = Disables the alerts.
 Config.disptachforall = false -- true = Enables the alerts for all meter robberies / false = Not sending alerts for all meter robberies
 Config.PoliceCallChance = 15 -- Chance of sending alerts to pd when someone robs meters. | Enabling Config.disptachforall will overide this.
